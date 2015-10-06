b1e883b34e19ca9fcdb03aec31fd50ba18bebac0
pikesley <github@orgraphone.org>
2015-10-06 16:56:19 +0100

    All of this moves to Skellington

--- definitions/precompile_assets.rb
+++
@@ -1,9 +0,0 @@
-define :precompile_assets, :params => {} do
-  bash 'Precompiling assets' do
-    cwd params[:cwd]
-    user params[:user]
-    code <<-EOF
-      RACK_ENV=production bundle exec rake assets:precompile > /tmp/first_run 2>&1
-    EOF
-  end
-end
