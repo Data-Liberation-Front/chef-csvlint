eec6cb3496dcedcbb39477e9ca0a9930dae76e42
pikesley <github@orgraphone.org>
2015-10-06 17:00:45 +0100

    Because we now include the pool size

--- test/integration/production/serverspec/config_spec.rb
+++ test/integration/production/serverspec/config_spec.rb
@@ -6,5 +6,5 @@ describe file '/home/csvlint/csvlint.io/shared/config/database.yml' do
   its(:content) { should match /production:/ }
   its(:content) { should match /adapter: mysql2/ }
   its(:content) { should match /password: csvlint/ }
-  its(:md5sum) { should eq 'ba9f5e83f95c1f9eb9682bc0537a5783' }
+  its(:md5sum) { should eq 'fbfe237f10d8b34299ae690def204d34' }
 end
