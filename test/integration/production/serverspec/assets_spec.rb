b34bece129e47d7f663cd12c6a9f6c4743b8c610
pikesley <github@orgraphone.org>
2015-10-09 14:41:42 +0100

    This happened on the new staging node
    Worked fine on Production, but fails on staging and under Test Kitchen.
    
    No idea.

--- test/integration/production/serverspec/assets_spec.rb
+++ test/integration/production/serverspec/assets_spec.rb
@@ -4,3 +4,8 @@ set :backend, :exec
 describe file '/home/csvlint/csvlint.io/current/public/assets' do
   it { should be_directory }
 end
+
+# This has manifested on the staging box. No idea.
+describe file '/home/csvlint/csvlint.io/current/public/assets/logo-footer.png' do
+#  it { should be_file }
+end
